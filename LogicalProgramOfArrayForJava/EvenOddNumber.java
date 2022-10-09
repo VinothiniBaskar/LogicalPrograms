fe36d550a5192429543d786212b9d43709719eaa
VinothiniBaskar <113995562+VinothiniBaskar@users.noreply.github.com>
2022-10-09 18:20:51 +0530

    Add files via upload

---
+++ LogicalProgramOfArrayForJava/EvenOddNumber.java
@@ -0,0 +1,16 @@
+package LogicalProgramOfArrayForJava;
+
+public class EvenOddNumber {
+
+	public static void main(String[] args) {
+		int a = 11;
+		if (a % 2 == 0) {
+			System.out.println("This is even number ");
+		} else {
+			System.out.println("This is odd number ");
+
+		}
+
+	}
+
+}
